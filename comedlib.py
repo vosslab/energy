2d744c06a66fdfd268b7f68bc3f7fef5ec384590
Neil Voss <vossman77@yahoo.com>
2018-12-12 18:39:27 -0600

    new cleaner API calls

--- comedlib.py
+++ comedlib.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+import os
 import sys
 import time
 import json
+import yaml
 import numpy
 import random
 import requests
@@ -19,25 +21,41 @@ CL = commonlib.CommonLib()
 #======================================
 class ComedLib(object):
 	def __init__(self):
-		self.cachefile = "comed_cache_file.yml"
+		scriptdir = os.path.dirname(__file__)
+		filename = "comed_cache_file.yml"
+		self.cachefile = os.path.join(scriptdir, filename)
 		self.baseurl = "https://hourlypricing.comed.com/api?type=5minutefeed"
 		#comedurl = "https://hourlypricing.comed.com/api?type=5minutefeed&datestart=201801030005&dateend=201801032300"
 		return
 	
 	#======================================
-	def writeCache(self):
+	def writeCache(self, data):
+		print("saving data to %s"%(self.cachefile))
 		f = open(self.cachefile, "w")
+		fulldata = {
+			'timestamp': int(time.time()),
+			'data': data,
+		}
+		yaml.dump(fulldata, f)
 		f.close()
-		pass
+		return
 
 	#======================================
 	def readCache(self):
+		if not os.path.exists(self.cachefile):
+			return None
+		print("reading data from %s"%(self.cachefile))
 		f = open(self.cachefile, "r")
+		fulldata = yaml.load(f)
 		f.close()
-		pass
+		if not isinstance(fulldata, dict):
+			return None
+		if time.time() - fulldata['timestamp'] > 240:
+			return None
+		return fulldata['data']
 
 	#======================================
-	def getUrl(self, url):
+	def safeDownloadWebpage(self, url):
 		fails = 0
 		while(fails < 9):
 			try:
@@ -56,39 +74,48 @@ class ComedLib(object):
 		if fails >= 9:
 			print "ERROR: too many failed requests"
 			sys.exit(1)
+		return resp
+
+	#======================================
+	def downloadComedJsonData(self, url=None):
+		data = self.readCache()
+		if isinstance(data, list):
+			print("Using comed data from cache")
+			return data
+		else:
+			print("Downloading comed new data")
+		if url is None:
+			url = self.getUrl()
+		resp = self.safeDownloadWebpage(url)
 		try:
 			data = json.loads(resp.text)
 		except ValueError:
 			time.sleep(300 + 100*random.random())
-			data = self.getUrl(url)
+			data = self.downloadComedJsonData(url)
+		self.writeCache(data)
 		time.sleep(random.random())
 		return data
 
 	#======================================
-	def getTodayUrl(self):
-		now = datetime.datetime.now()
-		timecode = "%04d%02d%02d0000"%(now.year, now.month, now.day)
-		print timecode
-		comedurl = self.baseurl+"&datestart="+timecode
-		return comedurl
+	def getUrl(self):
+		return self.baseurl
 
 	#======================================
 	def getHourUrl(self):
-		now = datetime.datetime.now()
-		timecode = "%04d%02d%02d%02d00"%(now.year, now.month, now.day, now.hour)
-		print timecode
-		comedurl = self.baseurl+"&datestart="+timecode
-		return comedurl
+		return self.getUrl()
+		### this does not work to limit data
+		#now = datetime.datetime.now()
+		#timecode = "%04d%02d%02d%02d00"%(now.year, now.month, now.day, now.hour)
+		#print timecode
+		#adding timecode does not work to limit data return, only good for past dates, e.g., 2014
+		#comedurl = self.baseurl #+"&datestart="+timecode
+		#return comedurl
 
 	#======================================
-	def getCurrentComedRate(self):
-		comedurl = self.getHourUrl()
-		data = None
-		while data is None:
-			data = self.getUrl(comedurl)
-		x = []
+	def parseComedData(self, data):
+		#x = []
+		#y = []
 		yvalues = {}
-		y = []
 		day = None
 		#print "Current Price %.2f"%(float(data[0]['price']))
 		for p in data:
@@ -111,9 +138,16 @@ class ComedLib(object):
 			except KeyError:
 				yvalues[hour] = [price,]
 				yvalues[hour2] = [price,]
-			x.append(hours)
-			y.append(price)
+			#x.append(hours)
+			#y.append(price)
+		return yvalues
 
+	#======================================
+	def getCurrentComedRate(self):
+		data = None
+		while data is None:
+			data = self.downloadComedJsonData()
+		yvalues = self.parseComedData(data)
 		x2 = yvalues.keys()
 		x2.sort()
 
@@ -125,9 +159,24 @@ class ComedLib(object):
 		ystd = ypositive.std()
 		weight = (13-len(ylist))/13.
 		if abs(key - float(int(key))) < 0.001:
-			print "%03d:00 -> %2.2f +- %2.2f / %2.2f -> %.1f/%.1f"%(key, ymean, ystd, ystd*weight, yarray.min(), yarray.max())
+			print("%03d:00 -> %2.2f +- %2.2f / %2.2f -> %.1f/%.1f"
+				%(key, ymean, ystd, ystd*weight, yarray.min(), yarray.max()))
 			pass
 		return ymean + ystd*weight
+
+	#======================================
+	def getMedianComedRate(self):
+		data = None
+		while data is None:
+			data = self.downloadComedJsonData()
+		prices = []
+		for item in data:
+			prices.append(float(item['price']))
+		parray = numpy.array(prices, dtype=numpy.float64)
+		median = numpy.median(parray)
+		std = numpy.std(parray)
+		print("24 hour median price: %.3f +/- %.3f"%(median, std))
+		return median, std
 
 #======================================
 #======================================
