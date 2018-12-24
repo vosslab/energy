796915ae9eb1ac32175cc6748f45a1a47e8791d4
Neil Voss <vossman77@yahoo.com>
2018-12-24 08:54:54 -0600

    take median of three values

--- logEnergy.py
+++ logEnergy.py
@@ -9,44 +9,52 @@ import time
 import smbus
 import solarProduction
 
-# Get I2C bus
-bus = smbus.SMBus(1)
-noOfChannel = 2
+def getCurrent():
+	# Get I2C bus
+	bus = smbus.SMBus(1)
+	noOfChannel = 2
+	command1 = [0x6A, 0x01, 0x01, 0x0C, 0x00, 0x00, 0x0A]
+	bus.write_i2c_block_data(0x2A, 0x92, command1)
+	time.sleep(0.1)
+	data1 = bus.read_i2c_block_data(0x2A, 0x55, noOfChannel*3 + 3)
+	# Convert the data
+	totalCurrent = 0
+	currents = []
+	for i in range(0, noOfChannel) :
+		msb1 = data1[i*3]
+		msb = data1[1 + i*3]
+		lsb = data1[2 + i*3]
+		# Convert the data to milliamperes
+		current = (msb1 * 65536 + msb * 256 + lsb)
+		currents.append(current)
+		totalCurrent += current
+	return currents, totalCurrent
 
-command1 = [0x6A, 0x01, 0x01, 0x0C, 0x00, 0x00, 0x0A]
-bus.write_i2c_block_data(0x2A, 0x92, command1)
+def getSolarCurrent():
+	solardata = solarProduction.getSolarUsage()
+	solarValue = float(solardata['Current Production']['Value'])
+	#convert from watts to milliamps
+	solarcurrent = int(round(solarValue/0.12))
+	return solarcurrent
 
-time.sleep(0.1)
+currents1, totalCurrent1 = getCurrent()
+solarcurrent1 = getSolarCurrent()
+currents2, totalCurrent2 = getCurrent()
+solarcurrent2 = getSolarCurrent()
+currents3, totalCurrent3 = getCurrent()
+solarcurrent3 = getSolarCurrent()
 
-data1 = bus.read_i2c_block_data(0x2A, 0x55, noOfChannel*3 + 3)
-
-# Convert the data
-totalCurrent = 0
+#take the median values
+solarcurrent = sorted([solarcurrent1, solarcurrent2, solarcurrent3])[1]
+totalCurrent = sorted([totalCurrent1, totalCurrent2, totalCurrent3])[1]
 currents = []
-for i in range(0, noOfChannel) :
-	msb1 = data1[i * 3]
-	msb = data1[1 + i * 3]
-	lsb = data1[2 + i * 3]
-
-	# Convert the data to milliamperes
-	current = (msb1 * 65536 + msb * 256 + lsb)
-
+for i in range(len(currents1)):
+	current = sorted([currents1[i], currents2[i], currents3[i]])[1]
 	currents.append(current)
-	totalCurrent += current
-
-#print "TOTAL"
-#print "  Current : %.2f A"%(totalCurrent)
-#print "  Usage   : %.2f kW"%(totalCurrent*0.120)
-
-solardata = solarProduction.getSolarUsage()
-solarValue = float(solardata['Current Production']['Value'])
-solarKilowatts = solarValue/1000.
-#convert from watts to milliamps
-solarcurrent = int(round(solarValue/0.12))
 
 datestamp = time.strftime("%Y-%m%b-%d-%a").lower()
 logname = "usage/%s.log"%(datestamp)
-print("Usage: %.3f kW use / %.3f kW solar --> %s"%(totalCurrent*120/1e6, solarKilowatts, logname))
+print("Usage: %.3f kW use / %.3f kW solar --> %s"%(totalCurrent*120/1e6, solarcurrent*120/1e6, logname))
 
 f = open(logname, "a")
 f.write("%d,%d,%d,%d,%d\n"%
