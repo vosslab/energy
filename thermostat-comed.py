d8983c3fd118c929bf3e0102cb36c1fc8c58c7f1
Neil Voss (dell box) <vossman77@yahoo.com>
2020-07-27 09:34:35 -0500

    add heat index

--- thermostat-comed.py
+++ thermostat-comed.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
 import sys
+import numpy
 import datetime
 import comedlib
 import ecobeelib
 
 class ThermoStat(object):
 	def __init__(self):
+		self.use_humid = True
 		self.hightemp = 80.1
-		self.cooltemp = 72.1
+		self.cooltemp = 73.1
 		self.comlib = comedlib.ComedLib()
 		self.comlib.msg = False
 		self.current_rate = None
@@ -43,7 +45,9 @@ class ThermoStat(object):
 		events_tree = self.myecobee.events()
 		user_override = False
 		for event_dict in events_tree:
-			if not event_dict['end_time'].endswith("01"):
+			if ( event_dict['cool_hold_temp'] % 10 != 1
+					and event_dict['is_cool_off'] is False
+					and not event_dict['end_time'].endswith("01") ):
 				user_override = True
 		return user_override
 
@@ -56,21 +60,65 @@ class ThermoStat(object):
 		else:
 			print("\nnothing to do")
 
+	def getRelativeHumidity(self):
+		humidvalues = []
+		sensorsdict = self.myecobee.sensors()
+		keys = list(sensorsdict.keys())
+		for name in keys:
+			humid = sensorsdict[name].get('humidity')
+			#print(list(sensorsdict[name].keys()))
+			if humid is not None:
+				humidvalues.append(humid)
+		if len(humidvalues) == 0:
+			print("Failed to get relative humidity")
+			return None
+
+		rel_humid = numpy.array(humidvalues).mean()
+		print("Relative humidity: {0:.1f}%".format(rel_humid))
+		return numpy.array(humidvalues).mean()
+
+	def getAdjustedCoolTemp(self):
+		if self.use_humid is False:
+			return self.cooltemp
+		rel_humid = self.getRelativeHumidity()
+		if rel_humid is None:
+			return self.cooltemp
+		adjustTemp = self.inverseHeatIndex(self.cooltemp, rel_humid)
+		#rounding
+		adjustTemp = round(adjustTemp, 1)
+		if int(adjustTemp*10) % 10 == 0:
+			adjustTemp += 0.1
+		adjustTemp = round(adjustTemp, 1)
+		print("Adjusted Cool Setting: {0:.1f}F".format(adjustTemp))
+		if adjustTemp > self.cooltemp:
+			adjustTemp = self.cooltemp
+		return adjustTemp
+
 	def turnOnEcobee(self):
 		print("Request: Turn ON air conditioner")
-		if self.coolsetting > self.cooltemp + 1:
+		adjustTemp = self.getAdjustedCoolTemp()
+		if self.coolsetting > adjustTemp + 1:
 			print("Request: Turn ON air conditioner")
-			print(("Set A/C to {0:.1f} F".format(self.cooltemp)))
-			self.myecobee.setTemperature(cooltemp=self.cooltemp, endTimeMethod='end_of_hour')
+			print(("Set A/C to {0:.1f} F".format(adjustTemp)))
+			self.myecobee.setTemperature(cooltemp=adjustTemp, endTimeMethod='end_of_hour')
 			#myecobee.sendMessage("A/C was set to 80F, because ComEd Prices are High -- Neil")
 		else:
 			print("\nnothing to do")
 
+	def heatIndex(self, temp, rel_humid):
+		heat_index = 1.1 * temp + 0.047 * rel_humid - 10.3
+		return heat_index
+
+	def inverseHeatIndex(self, heat_index, rel_humid):
+		temp = ( heat_index - 0.047 * rel_humid + 8.5)/1.1
+		return temp
+
+
 if __name__ == "__main__":
 
 	now = datetime.datetime.now()
-	if now.hour < 7 or now.hour >= 19:
-		print("only run program between 7 am and 7 pm => exit")
+	if now.hour < 6 or now.hour >= 22:
+		print("only run program between 6am and 10pm => exit")
 		sys.exit(0)
 
 	thermstat = ThermoStat()
@@ -78,8 +126,13 @@ if __name__ == "__main__":
 		print("user override in effect => exit")
 		sys.exit(0)
 
-	if now.minute <= 20:
-		print("less than 20 minutes past the hour => turn off")
+	if now.hour < 9 or now.hour >= 19:
+		time_cutoff = 20
+	else:
+		time_cutoff = 9
+
+	if now.minute <= time_cutoff:
+		print("less than {0:d} minutes past the hour => turn off".format(time_cutoff))
 		thermstat.turnOffEcobee()
 		sys.exit(0)
 
@@ -88,5 +141,3 @@ if __name__ == "__main__":
 		thermstat.turnOffEcobee()
 	else:
 		thermstat.turnOnEcobee()
-
-
