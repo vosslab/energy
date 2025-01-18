59a52eb8a8ea9823ee7fc52a0733614184b74106
Neil Voss <vossman77@yahoo.com>
2025-01-18 17:01:05 -0600

    split htmlComedData() into subfunctions, so more data

--- htmltools.py
+++ htmltools.py
@@ -1,7 +1,9 @@
+import math
 import time
 import numpy
 import colorsys
 import datetime
+from energylib import comedlib
 
 def numberToHtmlColor(hue, saturation=0.9, value=0.6):
 	hue = max(0, min(hue, 1))  # Clamp hue between 0 and 1
@@ -135,107 +137,208 @@ def htmlEcobee(weather=False):
 
 	return htmltext
 
+def htmlComedData(showPlot: bool = False) -> str:
+	"""
+	Generate HTML content summarizing ComEd electricity pricing data.
 
-def htmlComedData(showPlot=False):
+	Args:
+		showPlot (bool): Whether to include a plot image link in the HTML output.
+
+	Returns:
+		str: An HTML-formatted string containing ComEd pricing information.
+	"""
+
+	# Initialize HTML output with a header
 	htmltext = "<h3>Comed Prices</h3>"
 
-	import comedlib
-	comlib =	comedlib.ComedLib()
-	comlib.msg = False
-	comlib.useCache = False
+	# Set up the ComEd library instance
+	comlib = comedlib.ComedLib()
+	comlib.msg = False  # Suppress console messages from the library
+	comlib.useCache = False  # Disable caching to ensure fresh data is downloaded
+
+	# Download ComEd JSON data
 	comed_data = comlib.downloadComedJsonData()
 	if comed_data is None:
+		# If no data is available, display an error message in the HTML
 		htmltext += "comed data failed or not available"
 		return htmltext
 
-	gas_conversion_rate = 34.7/3.9
+	# Generate the status header, including median, current, and predicted rates
+	htmltext += _generate_status_header_html(comlib, comed_data)
 
-	htmltext += "<span style='color: &#35;448844'>24hr Median Rate:"
-	median,std = comlib.getMedianComedRate(comed_data)
-	htmltext += " {0} &pm; {1:.2f} &cent;</span><br/>".format(colorPrice(median, 1), std)
+	# Add a table displaying recent electricity rates
+	htmltext += _generate_recent_rates_table(comed_data)
 
-	htmltext += "&nbsp;<span style='color: &#35;448844'>Equivalent Gas Rate:"
-	med_gas_equiv = (median + 3.8) * gas_conversion_rate / 100.
-	std_gas_equiv = (std) * gas_conversion_rate / 100.
-	htmltext += "</span> ${0:.2f} &pm; {1:.2f} per gallon<br/>".format(med_gas_equiv,std_gas_equiv)
-
-	htmltext += "<span style='color: &#35;444488'>Hour Current Rate:"
-	currentRate = comlib.getCurrentComedRate(comed_data)
-	htmltext += " {0} </span><br/>".format(colorPrice(currentRate, 3))
-
-	htmltext += "&nbsp;<span style='color: &#35;444488'>Equivalent Gas Rate:"
-	gas_equiv = (currentRate + 3.8) * gas_conversion_rate / 100.
-	htmltext += "</span> ${0:.2f} per gallon<br/>".format(gas_equiv)
-
-	htmltext += "<span style='color: &#35;884444'>Hour Predict Rate:"
-	predictRate = comlib.getPredictedRate(comed_data)
-	htmltext += " {0} </span><br/>".format(colorPrice(predictRate, 3))
-
-	htmltext += "<span style='color: &#35;448844'>Usage CutOff Rate:"
-	cutoffRate = comlib.getReasonableCutOff()
-	htmltext += " {0} </span><br/>".format(colorPrice(cutoffRate, 3))
-
-	htmltext += "House Usage Status:\n"
-	htmltext += "<table style='display:inline-block; border: 1px solid lightgray; vertical-align:middle;'><tr>\n"
-	if predictRate < cutoffRate:
-		htmltext += "<td padding=10 bgcolor='darkgreen'><span style='color: white'><b>*ON*</b>\n"
-	else:
-		htmltext += "<td padding=10 bgcolor='darkred'><span style='color: white'><b>.OFF.</b>\n"
-	htmltext += "</span></td></tr></table>"
-	htmltext += "<br/>\n"
-
-	htmltext += "<table style='border: 1px solid darkblue; border-spacing: 3px; "
-	htmltext += "  display: inline-block; vertical-align: top;'>\n"
-	htmltext += "<tr><th colspan='2'>Recent Rates</th></tr>\n"
-	htmltext += "<tr><th>Time</th><th>Cost</th></tr>\n"
-	now = datetime.datetime.now()
-	minutes = now.minute
-	timepoints = max( int(minutes/5), 8)
-	number_of_rows = min( timepoints, len(comed_data) )
-	for item in comed_data[:number_of_rows]:
-		timestruct = list(time.localtime(int(item['millisUTC'])/1000.))
-		rate = float(item['price'])
-		#htmltext += "<li>%d:%02d &ndash; %s </li>"%(timestruct[3], timestruct[4], colorPrice(rate))
-		htmltext += "<tr>\n"
-		htmltext += "  <td align='center'> {0:d}:{1:02d} </td>\n".format(timestruct[3], timestruct[4])
-		htmltext += "  <td align='right'> {0} </td>\n".format(colorPrice(rate, 1))
-		htmltext += "</tr>\n"
-	htmltext += "</table>\n"
-	#htmltext += "<br/>\n"
-	htmltext += "&nbsp;\n"
-
+	# Add a table displaying hourly averages
 	hourlyRates = comlib.parseComedData(comed_data)
+	htmltext += _generate_hourly_averages_table(hourlyRates)
 
-	htmltext += "<table style='border: 1px solid darkblue; border-spacing: 3px; "
-	htmltext += "  display: inline-block; vertical-align: top;'>\n"
-	htmltext += "<tr><th colspan='2'>Hourly Averages</th></tr>\n"
-	htmltext += "<tr><th>Range</th><th>Cost</th></tr>\n"
-	keys = list(hourlyRates.keys())
-	keys.sort()
-	keys.reverse()
-	for key in keys:
-		if int(key) < 1:
-			continue
-		#htmltext += "'%s'<br/>\n"%(key))
-		if abs(float(int(key)) - float(key)) > 1e-6:
-			continue
-
-		averageRate = numpy.array(hourlyRates[key]).mean()
-		hour = int(key)
-		#timestruct = list(time.localtime(int(item['millisUTC'])/1000.))
-		#rate = float(item['price'])
-		htmltext += "<tr>\n"
-		htmltext += "  <td align='center'> {0:d}-{1:d}:00 </td>\n".format(hour-1, hour)
-		htmltext += "  <td align='right'> {0} </td>\n".format(colorPrice(averageRate, 2))
-		htmltext += "</tr>\n"
-	htmltext += "</table>\n"
-	htmltext += "<br/>\n"
-
-	htmltext += "<a href='energylib/plot_comed.py'>\n"
-	if showPlot is True:
-		htmltext += "<img src='energylib/plot_comed.py'>"
-		htmltext += "<br/>\n"
-	htmltext += "Show Comed Price Plot</a>\n"
+	# Optionally include a plot of ComEd rates as an image in the HTML
+	if showPlot:
+		htmltext += "<img src='energylib/plot_comed.py'><br/>\n"
+	htmltext += "<a href='energylib/plot_comed.py'>Show Comed Price Plot</a>\n"
 
 	return htmltext
+
+#==============
+# Helper Subfunctions
+#==============
+
+def _generate_status_header_html(comlib, comed_data) -> str:
+	"""
+	Generate HTML for the status header, which includes:
+	- Median electricity rate and its equivalent gas rate
+	- Current electricity rate and its equivalent gas rate
+	- Predicted electricity rate
+	- Usage cutoff rate
+	- Status of house electricity usage (ON/OFF)
+
+	Args:
+		comlib: An instance of the ComEd library.
+		comed_data: JSON data containing ComEd electricity pricing.
+
+	Returns:
+		str: HTML-formatted string containing the status header information.
+	"""
+	# Define gas-to-electricity conversion rate (predefined constant)
+	gas_conversion_rate = 34.7 / 3.9
+
+	# Get median and standard deviation of electricity rates
+	median, std = comlib.getMedianComedRate(comed_data)
+	# Get the current electricity rate
+	currentRate = comlib.getCurrentComedRate(comed_data)
+	# Get the predicted electricity rate and cutoff rate for usage
+	predictRate = comlib.getPredictedRate(comed_data)
+	cutoffRate = comlib.getReasonableCutOff()
+
+	# Generate the HTML content
+	html = ""
+	html += "<span style='color: &#35;448844'>24hr Median Rate:"
+	html += f" {colorPrice(median, 1)} &pm; {std:.2f} &cent;</span><br/>"
+
+	html += "&nbsp;<span style='color: &#35;448844'>Equivalent Gas Rate:"
+	med_gas_equiv = (median + 3.8) * gas_conversion_rate / 100.0
+	std_gas_equiv = std * gas_conversion_rate / 100.0
+	html += f"</span> ${med_gas_equiv:.2f} &pm; {std_gas_equiv:.2f} per gallon<br/>"
+
+	html += "<span style='color: &#35;444488'>Hour Current Rate:"
+	html += f" {colorPrice(currentRate, 3)} </span><br/>"
+
+	html += "&nbsp;<span style='color: &#35;444488'>Equivalent Gas Rate:"
+	gas_equiv = (currentRate + 3.8) * gas_conversion_rate / 100.0
+	html += f"</span> ${gas_equiv:.2f} per gallon<br/>"
+
+	html += "<span style='color: &#35;884444'>Hour Predict Rate:"
+	html += f" {colorPrice(predictRate, 3)} </span><br/>"
+
+	html += "<span style='color: &#35;448844'>Usage CutOff Rate:"
+	html += f" {colorPrice(cutoffRate, 3)} </span><br/>"
+
+	# Display house usage status (ON/OFF) based on predicted vs. cutoff rates
+	html += "House Usage Status:\n"
+	html += "<table style='display:inline-block; border: 1px solid lightgray; vertical-align:middle;'><tr>\n"
+	if predictRate < cutoffRate:
+		html += "<td padding=10 bgcolor='darkgreen'><span style='color: white'><b>*ON*</b>\n"
+	else:
+		html += "<td padding=10 bgcolor='darkred'><span style='color: white'><b>.OFF.</b>\n"
+	html += "</span></td></tr></table>"
+	html += "<br/>\n"
+
+	return html
+
+def _generate_recent_rates_table(comed_data: list) -> str:
+	"""
+	Generate an HTML table for recent electricity rates.
+
+	Args:
+		comed_data (list): List of ComEd electricity rate data.
+
+	Returns:
+		str: HTML-formatted string for the recent rates table.
+	"""
+	html = ""
+	html += "<table style='border: 1px solid darkblue; border-spacing: 3px; "
+	html += "  display: inline-block; vertical-align: top;'>\n"
+	html += "<tr><th colspan='2'>Recent Rates</th></tr>\n"
+	html += "<tr><th>Time</th><th>Cost</th></tr>\n"
+
+	# Calculate rows to display with actual data
+	now = datetime.datetime.now()
+	minutes = now.minute
+	timepoints = max(int(minutes / 5), 8)  # Minimum 8 timepoints, based on 5-minute intervals
+	number_of_rows = min(timepoints, len(comed_data))  # Don't exceed the length of available data
+
+	# Display rows with actual data
+	for i, item in enumerate(comed_data[:number_of_rows]):
+		timestruct = list(time.localtime(int(item['millisUTC']) / 1000.0))
+		rate = float(item['price'])
+		html += "<tr>\n"
+		html += f"  <td align='center'> {timestruct[3]:d}:{timestruct[4]:02d} </td>\n"
+		html += f"  <td align='right'> {colorPrice(rate, 1)} </td>\n"
+		html += "</tr>\n"
+
+	# Display additional rows in gray if there are more timepoints available
+	if len(comed_data) > number_of_rows:
+		for i in range(number_of_rows, min(23, len(comed_data))):
+			item = comed_data[i]
+			timestruct = list(time.localtime(int(item['millisUTC']) / 1000.0))
+			rate = float(item['price'])
+			html += "<tr style='background-color: lightgray;'>\n"
+			html += f"  <td align='center'> {timestruct[3]:d}:{timestruct[4]:02d} </td>\n"
+			html += f"  <td align='right'> {colorPrice(rate, 1)} </td>\n"
+			html += "</tr>\n"
+
+	html += "</table>\n"
+	html += "&nbsp;\n"
+
+	return html
+
+
+def _generate_hourly_averages_table(hourlyRates: dict) -> str:
+	"""
+	Generate an HTML table for hourly average electricity rates.
+
+	Args:
+		hourlyRates (dict): Dictionary of hourly rates, with keys as hours
+		                    and values as lists of rates for each hour.
+
+	Returns:
+		str: HTML-formatted string for the hourly averages table.
+	"""
+	html = "<table style='border: 1px solid darkblue; border-spacing: 3px; "
+	html += "  display: inline-block; vertical-align: top;'>\n"
+	html += "<tr><th colspan='2'>Hourly Averages</th></tr>\n"
+	html += "<tr><th>Range</th><th>Cost</th></tr>\n"
+
+	# Sort hour keys in descending order
+	hour_keys = list(hourlyRates.keys())
+	hour_keys.sort(reverse=True)
+
+	# Generate rows for hourly averages
+	for hour_key in hour_keys:
+		# Skip invalid keys that are not close to integers
+		if not math.isclose(hour_key, round(hour_key), abs_tol=1e-6):
+			continue
+
+		# Calculate average rate for the current hour
+		averageRate = numpy.array(hourlyRates[hour_key]).mean()
+		hour = int(hour_key)
+
+		# Determine background color for rows after midnight
+		if hour < 1:
+			bg_color = "lightgray"
+			hour += 24  # Adjust hour to represent a full 24-hour format
+		else:
+			bg_color = "white"
+
+		# Generate HTML for the row
+		html += f"<tr style='background-color: {bg_color};'>\n"
+		html += f"  <td align='center'> {hour - 1:d}-{hour:d}:00 </td>\n"
+		html += f"  <td align='right'> {colorPrice(averageRate, 2)} </td>\n"
+		html += "</tr>\n"
+
+	html += "</table>\n"
+	html += "<br/>\n"
+
+	return html
 
