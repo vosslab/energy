cf5bdc7c82ae21c339822e36a46ddb91dbb39360
Neil Voss <vossman77@yahoo.com>
2025-02-20 15:40:06 -0600

    allow static generation

--- html/comed.py
+++ html/comed.py
@@ -1,35 +1,48 @@
 #!/usr/bin/env python3
 
-#Traceback manager for CGI scripts
-import cgitb
-cgitb.enable()
-
 import sys
-sys.path.append('/home/pi/energy')
-
 import time
+import cgitb
 import numpy
 from energylib import comedlib
 from energylib import htmltools
 
-print("Content-Type: text/html\n")
-print("\n")
-print("<head>")
-print("<title>Comed Hourly Prices</title>")
-print("</head>")
+# Enable traceback debugging for CGI
+cgitb.enable()
 
-#======================================
-print("<body>")
-print("<h1>Comed Hourly Prices</h1>")
-print("<a href='house.py'>Show All Energy Data</a><br/>")
-print("<h3>Current time:</h3>%s"%(time.asctime()))
-print('<br/>\n')
+sys.path.append('/home/pi/energy')
 
-#======================================
-#======================================
-print(htmltools.htmlComedData())
+def generate_html(output_file=None):
+	"""
+	Generates the Comed Hourly Prices HTML page.
+	If `output_file` is specified, writes the output to a file instead of printing.
+	"""
+	html_content = """<!DOCTYPE html>
+<html>
+<head>
+    <title>Comed Hourly Prices</title>
+</head>
+<body>
+    <h1>Comed Hourly Prices</h1>
+    <a href='house.py'>Show All Energy Data</a><br/>
+    <h3>Current time:</h3> {current_time}
+    <br/>
+    {comed_data}
+</body>
+</html>""".format(
+		current_time=time.asctime(),
+		comed_data=htmltools.htmlComedData()
+	)
 
+	# If an output file is provided, write to it
+	if output_file:
+		with open(output_file, "w") as f:
+			f.write(html_content)
+	else:
+		# CGI mode - print to standard output
+		print("Content-Type: text/html\n")
+		print(html_content)
 
-#======================================
-#======================================
-print("</body></html>")
+if __name__ == '__main__':
+	# If called directly, generate a static file instead of CGI output
+	generate_html("/var/www/html/comed.html")  # Adjust the path as needed
